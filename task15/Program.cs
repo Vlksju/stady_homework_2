6e426c2d59f6bb172efecd5d758f467b0cc5356b
Vlksju <xenya.vol2017@yandex.ru>
2022-07-30 22:48:16 +0500

    Solved task 15

---
+++ task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Программа которая определяет буднитй или");
+Console.WriteLine("выходной по номеру дня неделт.");
+
+Console.WriteLine("Введите номер дня недели");
+
+int number1 = Convert.ToInt32(Console.ReadLine ());
+
+if (number1 >= 8) {
+    Console.WriteLine("Дня недели с таким номером не существует");
+}
+else {
+    if (number1 == 6 | number1 == 7) {
+       Console.WriteLine("Выходной день"); 
+    }
+    else {
+        Console.WriteLine("Будний день");
+    }
+}

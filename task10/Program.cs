96f3fdd7a5687948c1cc1f26bbd36996a9d0c9fb
Vlksju <xenya.vol2017@yandex.ru>
2022-07-30 21:52:18 +0500

    Solved task 10

---
+++ task10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Программа которая вычисляет вторую цифру трехзначного числа");
+Console.WriteLine("Введите трехзначное число");
+
+int number1 = Convert.ToInt32(Console.ReadLine ());
+
+int digit = number1 /10 % 10;
+
+Console.WriteLine(digit);

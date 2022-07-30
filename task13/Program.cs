7af0e86115e6fae19aa560ba27cca89e0a250366
Vlksju <xenya.vol2017@yandex.ru>
2022-07-30 22:32:24 +0500

    Solved task 13

---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Программа которая выводит третью цифру числа");
+Console.WriteLine("Введите число");
+
+int number1 = Convert.ToInt32(Console.ReadLine ());
+
+number1 = Math.Abs (number1);
+
+if (number1<100){
+    Console.WriteLine("В числе нет третьей цифры");
+}
+else {
+    while (number1 > 999) {
+        number1 = number1 / 10;
+    }
+    Console.WriteLine (number1 % 10);
+}
